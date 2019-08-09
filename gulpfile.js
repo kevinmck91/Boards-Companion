d75f17be31296d3c14b19052a3f16971713b8353
Sean <seanmcloughlin9@gmail.com>
2019-08-10 00:12:26 +0100

    added firefox build

--- gulpfile.js
+++ gulpfile.js
@@ -2,22 +2,19 @@ const { src, series, dest } = require('gulp');
 const jsonTransform = require('gulp-json-transform');
 
 function copyE2eTestFiles() {
-    return src("./src/*")
-        .pipe(dest("dist/e2e-test"));
+    return src("./src/**")
+        .pipe(dest("dist/e2e-test/Chrome"))
+        .pipe(dest("dist/e2e-test/Firefox"));
 }
 
 function transformE2eManifest() {
     return src("./src/manifest.json")
         .pipe(jsonTransform(function (json, file) {
-            json.content_scripts[0].matches = "<all_urls>";
+            json.content_scripts[0].matches[0] = "<all_urls>";
             return JSON.stringify(json, null, '\t');
         }))
-        .pipe(dest("dist/e2e-test"));
+        .pipe(dest("dist/e2e-test/Chrome"))
+        .pipe(dest("dist/e2e-test/Firefox"));
 }
 
-function copyReleaseFiles() {
-    return src("./src/*")
-        .pipe(dest("dist/release"));
-}
-
-exports.default = series(copyE2eTestFiles, transformE2eManifest, copyReleaseFiles);
+exports.default = series(copyE2eTestFiles, transformE2eManifest);
