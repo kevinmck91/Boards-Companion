b882907e25e70a969c6383e304eca1475ee64cf1
Sean <seanmcloughlin9@gmail.com>
2019-08-17 15:43:40 +0100

    fixes to get firefox working

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,52 @@
 const { src, series, dest } = require('gulp');
 const jsonTransform = require('gulp-json-transform');
+const rollup = require('rollup-stream');
+const del = require('del');
+const source = require('vinyl-source-stream');
+
+function clean(cb) {
+    del('dist/e2e-test/Firefox/*');
+    del('dist/e2e-test/Chrome/*')
+    del('dist/release/Firefox/*');
+    del('dist/release/Chrome/*')
+    cb();
+}
+
+function copyChromeReleaseFiles() {
+    return src("./src/**")
+        .pipe(dest("dist/release/Chrome"));
+}
+
+function copyFirefoxReleaseFiles(cb) {
+    rollup({
+        input: './src/main.js',
+        format: 'iife'
+    })
+        .pipe(source('bundle.js'))
+        .pipe(dest('dist/release/Firefox'));
+
+    src("./src/css/**")
+        .pipe(dest("dist/release/Firefox/css"));
+
+    src("./src/fontawesome/**")
+        .pipe(dest("dist/release/Firefox/fontawesome"));
+
+    src("./src/icons/**")
+        .pipe(dest("dist/release/Firefox/icons"));
+
+    src("./src/popup/**")
+        .pipe(dest("dist/release/Firefox/popup"));
+
+    src("./src/manifest.json")
+        .pipe(dest("dist/release/Firefox"));
+
+    cb();
+}
 
 function copyE2eTestFiles() {
     return src("./src/**")
         .pipe(dest("dist/e2e-test/Chrome"))
         .pipe(dest("dist/e2e-test/Firefox"));
-}
-
-function copyReleaseFiles() {
-    return src("./src/**")
-        .pipe(dest("dist/release/Chrome"))
-        .pipe(dest("dist/release/Firefox"));
 }
 
 function transformE2eManifest() {
@@ -33,13 +69,14 @@ function transformE2eFirefoxManifest() {
         .pipe(dest("dist/e2e-test/Firefox"));
 }
 
-function transformReleaseFirefoxManifest() {
-    return src("dist/release/Firefox/manifest.json")
+function transformReleaseFirefoxManifest(cb) {
+    src("dist/release/Firefox/manifest.json")
         .pipe(jsonTransform(function (json, file) {
-            json.content_scripts[0].js[0] = "modules-load/Firefox/loadMain.js"
+            json.content_scripts[0].js[0] = "bundle.js"
             return JSON.stringify(json, null, '\t');
         }))
         .pipe(dest("dist/release/Firefox"));
+    cb();
 }
 
-exports.default = series(copyE2eTestFiles, transformE2eManifest, transformE2eFirefoxManifest, copyReleaseFiles, transformReleaseFirefoxManifest);
+exports.default = series(clean, copyE2eTestFiles, transformE2eManifest, transformE2eFirefoxManifest, copyFirefoxReleaseFiles, copyChromeReleaseFiles, transformReleaseFirefoxManifest);
