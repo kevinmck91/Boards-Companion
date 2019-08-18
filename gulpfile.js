78713906e0ce2741735c9be8c85a76881030afcc
Sean <seanmcloughlin9@gmail.com>
2019-08-18 13:18:46 +0100

    fixes for build

--- gulpfile.js
+++ gulpfile.js
@@ -4,52 +4,59 @@ const rollup = require('rollup-stream');
 const del = require('del');
 const source = require('vinyl-source-stream');
 
-function clean(cb) {
-    del('dist/e2e-test/Firefox/*');
-    del('dist/e2e-test/Chrome/*')
-    del('dist/release/Firefox/*');
-    del('dist/release/Chrome/*')
-    cb();
+function clean() {
+    return Promise.all([
+        del('dist/e2e-test/Firefox/*'),
+        del('dist/e2e-test/Chrome/*'),
+        del('dist/release/Firefox/*'),
+        del('dist/release/Chrome/*')
+    ]);
 }
 
-function copyChromeReleaseFiles() {
-    return src("./src/**")
-        .pipe(dest("dist/release/Chrome"));
+function copyChromeFiles(destinationFolder) {
+    return copyGlob("./src/**", destinationFolder);
 }
 
-function copyFirefoxReleaseFiles(cb) {
-    rollup({
+function rollupFirefoxMain(destinationFolder) {
+    return rollup({
         input: './src/main.js',
         format: 'iife'
     })
         .pipe(source('bundle.js'))
-        .pipe(dest('dist/release/Firefox'));
-
-    src("./src/css/**")
-        .pipe(dest("dist/release/Firefox/css"));
-
-    src("./src/fontawesome/**")
-        .pipe(dest("dist/release/Firefox/fontawesome"));
-
-    src("./src/icons/**")
-        .pipe(dest("dist/release/Firefox/icons"));
-
-    src("./src/popup/**")
-        .pipe(dest("dist/release/Firefox/popup"));
-
-    src("./src/storage/**")
-        .pipe(dest("dist/release/Firefox/storage"));
-
-    src("./src/manifest.json")
-        .pipe(dest("dist/release/Firefox"));
-
-    cb();
+        .pipe(dest(destinationFolder));
 }
 
-function copyE2eTestFiles() {
-    return src("./src/**")
-        .pipe(dest("dist/e2e-test/Chrome"))
-        .pipe(dest("dist/e2e-test/Firefox"));
+function copyGlob(glob, destinationFolder) {
+    return new Promise(function (resolve, reject) {
+        src(glob)
+            .pipe(dest(destinationFolder).on('end', resolve))
+    });
+}
+
+function copyFirefoxFiles(destinationFolder) {
+    return Promise.all([
+        rollupFirefoxMain(destinationFolder),
+        copyGlob("./src/css/**", destinationFolder + "/css"),
+        copyGlob("./src/fontawesome/**", destinationFolder + "/fontawesome"),
+        copyGlob("./src/icons/**", destinationFolder + "/icons"),
+        copyGlob("./src/popup/**", destinationFolder + "/popup"),
+        copyGlob("./src/storage/**", destinationFolder + "/storage"),
+        copyGlob("./src/manifest.json", destinationFolder)
+    ]);
+}
+
+function copyE2eFiles() {
+    return Promise.all([
+        copyChromeFiles("dist/e2e-test/Chrome"),
+        copyFirefoxFiles('dist/e2e-test/Firefox/')
+    ]);
+}
+
+function copyReleaseFiles() {
+    return Promise.all([
+        copyChromeFiles("dist/release/Chrome"),
+        copyFirefoxFiles('dist/release/Firefox')
+    ]);
 }
 
 function transformE2eManifest() {
@@ -63,23 +70,34 @@ function transformE2eManifest() {
         .pipe(dest("dist/e2e-test/Firefox"));
 }
 
-function transformE2eFirefoxManifest() {
-    return src("dist/e2e-test/Firefox/manifest.json")
-        .pipe(jsonTransform(function (json, file) {
-            json.content_scripts[0].js[0] = "modules-load/Firefox/loadMain.js"
-            return JSON.stringify(json, null, '\t');
-        }))
-        .pipe(dest("dist/e2e-test/Firefox"));
+function transformE2eFirefoxManifest(cb) {
+    return transformFirefoxManifest("dist/e2e-test/Firefox");
 }
 
 function transformReleaseFirefoxManifest(cb) {
-    src("dist/release/Firefox/manifest.json")
-        .pipe(jsonTransform(function (json, file) {
-            json.content_scripts[0].js[0] = "bundle.js"
-            return JSON.stringify(json, null, '\t');
-        }))
-        .pipe(dest("dist/release/Firefox"));
-    cb();
+    return transformFirefoxManifest("dist/release/Firefox");
 }
 
-exports.default = series(clean, copyE2eTestFiles, transformE2eManifest, transformE2eFirefoxManifest, copyFirefoxReleaseFiles, copyChromeReleaseFiles, transformReleaseFirefoxManifest);
+function transformFirefoxManifest(manifestFolder) {
+    return src(manifestFolder + "/manifest.json")
+        .pipe(jsonTransform(function (json, file) {
+            json.content_scripts[0].js[0] = "bundle.js";
+            json.browser_specific_settings = {
+                "gecko": {
+                    "id": "boards-companion@mozilla.org"
+                }
+            };
+            json.content_scripts[0].js[2] = "fontawesome/firefox-fix.js";
+            return JSON.stringify(json, null, '\t');
+        }))
+        .pipe(dest(manifestFolder));
+}
+
+exports.default = series(
+    clean,
+    copyE2eFiles,
+    transformE2eManifest,
+    transformE2eFirefoxManifest,
+    copyReleaseFiles,
+    transformReleaseFirefoxManifest
+);
